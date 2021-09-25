ad03f11d70055ec13fc99f505a3171da5db30715
sachinit254 <sachinit254@gmail.com>
2021-09-25 11:23:50 +0530

    Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import LandingPage from './screens/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      Hey !!
-    </div>
+      <>
+       <Header />
+       <main>
+         <LandingPage />
+       </main>
+       <Footer />
+      </>
   );
 }
 
---
+++ frontend/src/screens/LandingPage/LandingPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to the Notes App</h1>
+              <p className="subtitle">One safe place for all your notes..</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                >
+                  Register
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingPage;
